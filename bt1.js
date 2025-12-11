f55dc6896bae4017fe5bcc80b152c2dbacb1dc2b
nguyendinh11a6 <149230656+nguyendinh11a6@users.noreply.github.com>
2025-12-11 14:30:36 +0700

    Update bt1.js

--- bt1.js
+++ bt1.js
@@ -1,13 +1,9 @@
-// Khai báo biến
 let studentName = "Nguyễn Định";
 let birthYear = 2007;
 let gender = "Nam";
 
-// Khai báo hằng số cho năm hiện tại
 const CURRENT_YEAR = 2025;
 
-// Tính tuổi
 let age = CURRENT_YEAR - birthYear;
 
-// In ra thông tin
 console.log("Tên: " + studentName + ", Năm sinh: " + birthYear + ", Giới tính: " + gender + ", Tuổi: " + age);
