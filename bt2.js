824995a0ec013de8828d4a3ca9ded4192a97dfee
nguyendinh11a6 <149230656+nguyendinh11a6@users.noreply.github.com>
2025-12-11 14:34:11 +0700

    Comment out hoisting test variable logs
    Comment out console.log statements for hoisting variables.

--- bt2.js
+++ bt2.js
@@ -1,11 +1,11 @@
-console.log(hoistingTestVar);
+//console.log(hoistingTestVar);
 var hoistingTestVar = 10;
 console.log(hoistingTestVar);
 
-// console.log(hoistingTestLet); 
+//console.log(hoistingTestLet); 
 let hoistingTestLet = 20;
 console.log(hoistingTestLet);
 
-// console.log(hoistingTestConst);
+//console.log(hoistingTestConst);
 const hoistingTestConst = 30;
 console.log(hoistingTestConst);
